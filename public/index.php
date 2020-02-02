cea02e940a0ca14bd267d4733d65ada453162e8e
Jan Stanek <jan.stanek@upol.cz>
2020-02-02 17:31:25 +0100

    git init

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="cs">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>OLC</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+</head>
+<body>
+
+<div class="container">
+
+
+</div>
+
+</body>
+</html>
